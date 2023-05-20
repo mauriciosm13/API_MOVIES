167f4f15a09a56200a5cc0c87690ed6ad8b35ac5
Maurício Mendonça <70268675+mauriciosm13@users.noreply.github.com>
2023-05-19 21:07:13 -0300

    Commit GET

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -10,10 +10,18 @@ public class FilmeController : ControllerBase
     private static List<Movie> filmes = new List<Movie>();
 
     [HttpPost]
-    public void AddMovie(Movie filme)
+    public void AddMovie([FromBody] Movie filme)
     {
-        filmes.Add(filme);
-        Console.WriteLine(filme.Title);
-        Console.WriteLine(filme.Duration);
+        
+            filmes.Add(filme);
+            Console.WriteLine(filme.Title);
+            Console.WriteLine(filme.Duration);
+       
+    }
+
+    [HttpGet]
+    public IEnumerable<Movie> GetMovies()
+    { 
+        return filmes;
     }
 }
--- FilmesApi/FilmesApi/Models/Movie.cs
+++ FilmesApi/FilmesApi/Models/Movie.cs
@@ -1,10 +1,17 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "O id é obrigatorio")]
     public int Id { get; set; }
+    [Required(ErrorMessage = "O Título é obrigatorio")]
     public string Title { get; set; }
+    [Required(ErrorMessage = "O Genero é obrigatorio")]
+    [MaxLength(50, ErrorMessage = "Tamanho Máximo de caracteres é de 50")]
     public string Gender { get; set; }
-    public string Duration { get; set; }
+    [Required(ErrorMessage = "A Duração é obrigatorio")]
+    public int Duration { get; set; }
 }
 
